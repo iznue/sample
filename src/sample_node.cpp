55400b83f24bee7440ec7c912cf104fc739c528b
ola31 <abrahamola80@gmail.com>
2021-11-04 05:02:08 +0900

    first commit(2021-11-04)

---
+++ src/sample_node.cpp
@@ -0,0 +1,81 @@
+/*
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "sample_node");
+  ros::NodeHandle nh;
+
+  ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("chatter", 1000);
+
+  ros::Rate loop_rate(10);
+  while (ros::ok())
+  {
+    std_msgs::String msg;
+    msg.data = "hello world";
+
+    chatter_pub.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+  }
+
+  return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+  FILE *fp;
+  int way, oper;
+  unsigned int time;
+  double lat, lon, alt;
+
+  way = 1;
+  time = 151430;
+  lat = 36.668422;
+  lon = 128.351391;
+  alt = 71.50;
+
+  fp = fopen("sample.trj", "wb");
+
+  fwrite(&way, 1, sizeof(way), fp);
+  fwrite(&time, 1, sizeof(time), fp);
+  fwrite(&lat, 1, sizeof(lat), fp);
+  fwrite(&lon, 1, sizeof(lon), fp);
+  fwrite(&alt, 1, sizeof(alt), fp);
+  fclose(fp);
+  return 0;
+}
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char **argv)
+{
+  FILE *fp;
+  int way, oper;
+  unsigned int time;
+  double lat, lon, alt;
+  //fp = fopen("sample.trj", "rb");
+  fp = fopen("gpsdata_little_endian.trj", "rb");
+  while(!feof(fp)) {
+    fread(&way, 1, sizeof(int), fp);
+    fread(&time, 1, sizeof(int), fp);
+    fread(&lat, 1, sizeof(double), fp);
+    fread(&lon, 1, sizeof(double), fp);
+    fread(&alt, 1, sizeof(double), fp);
+    printf("%d\t%d\t\%.6f\t%.6f\t%.2f\n", way, time, lat, lon, alt);
+  }
+  fclose(fp);
+  return 0;
+}
+
+
